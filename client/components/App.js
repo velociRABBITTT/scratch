c993747e6fd7e7550ee741911be0a8f2ca75f41e
George <iSaySureWhyNot@gmail.com>
2021-08-13 19:15:05 -0400

    also submitted greeting

--- client/components/App.js
+++ client/components/App.js
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div>
       Hello there from Wes!!!
+      sup from George...
       <h1 className="header"> Team wonder pussies for the win</h1>
     </div>
   );
