ef92d247e204397c00a8e1b1d270778257d08857
iSaySureWhyNot <87236316+iSaySureWhyNot@users.noreply.github.com>
2021-08-13 19:21:06 -0400

    Revert "George/thing"

--- client/components/App.js
+++ client/components/App.js
@@ -3,9 +3,8 @@ import React, { Component } from "react";
 const App = () => {
   return (
     <div>
-      Hello there from Wes!!!
-      sup from George...
-      <h1 className="header"> Team wonder pussies for the win</h1>
+      Hello Wes! You successfully bundled!
+      <h1 className="header"> Team wonder puss</h1>
     </div>
   );
 };
