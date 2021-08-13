acb1701f4ebd28e06b1165334808f326c5ac3dbf
dominicd17 <dominicd17@gmail.com>
2021-08-13 15:17:20 -0700

    first commit - test

--- client/components/App.js
+++ client/components/App.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 const App = () => {
   return (
     <div>
-      Hello Wes! You successfully bundled!
-      <h1 className="header"> Team wonder puss</h1>
+      Hello there
+      <h1 className="header"> Team wonder pussies for the win</h1>
     </div>
   );
 };
