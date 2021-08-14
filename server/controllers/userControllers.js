0df40d33a2149006c005ada15818d8dbe2341629
wmungal <lagnum22@gmail.com>
2021-08-13 22:55:23 -0400

    updated file organization in backend, moved connections to modelfile

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,7 +1,19 @@
 const User = require("../models/userModel");
 const userController = {};
 
-userController.createUser = (req, res, next) => {};
+userController.createUser = (req, res, next) => {
+  const newUser = new User(req.body);
+  
+  User.create(newUser)
+    .save()
+    .then((data) => {
+      res.locals.user = data;
+      return next();
+    })
+    .catch((err) =>
+      next({ message: `userController.createUser: Error: ${err}` })
+    );
+};
 
 userController.verifyUser = (req, res, next) => {};
 module.exports = userController;
