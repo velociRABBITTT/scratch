fb90821e23cdc1655678e586732a53254296f266
George <iSaySureWhyNot@gmail.com>
2021-08-13 21:08:54 -0400

    updated postModel

--- server/models/postModel.js
+++ server/models/postModel.js
@@ -5,9 +5,8 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema({
   goal: { type: String, required: true },
   method: { type: String },
-  results: { type: String },
   duration: { type: String },
-  conclusion: { type: String }
+  results: { type: String }
 });
 
 module.exports = mongoose.model("Post", postSchema);
