b81b704fa7200b40a09984b0f9139a56b8fe099e
Tyler Holt <tylerprestonholt@gmail.com>
2021-08-18 16:47:52 -0400

    create component to render login page

---
+++ client/components/Login.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Button, TextField, Grid, Container } from '@material-ui/core';
+  // Paper,
+  // AppBar,
+  // Typography,
+  // Toolbar,
+  // Link,
+  // } from "@material-ui/core";
+import { StylesProvider, styled, /*ThemeProvider*/ } from '@material-ui/core/styles';
+
+const LoginButton = styled(Button)({
+  background: '#DE8B47'
+});
+
+const CreateUserButton = styled(Button)({
+  background: '#5E80DF'
+});
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+    <StylesProvider injectFirst>
+      <div id ='login'>
+        <Container maxWidth="xs" >
+          <img id="logo" src="https://cdn.discordapp.com/attachments/876099998331322400/876861169980293140/comp_12.png"></img>
+          <form>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <TextField id='lUser' fullWidth label="Username" name="username" size="small" variant="outlined" />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      id='lPass'
+                      fullWidth
+                      label="Password"
+                      name="password"
+                      size="small"
+                      type="password"
+                      variant="outlined"
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={12}>
+                <LoginButton fullWidth id='loginButton' variant="contained" onClick={this.props.handleLogin}>
+                  Log in
+                </LoginButton>
+                <CreateUserButton color="primary" id='createUser' className='createUser' fullWidth variant="contained" onClick={this.props.handleCreate}>
+                  Create User
+                </CreateUserButton>
+              </Grid>
+            </Grid>
+          </form>
+        </Container>
+      </div>
+    </StylesProvider>
+    );
+  }
+}
+
+export default Login;
